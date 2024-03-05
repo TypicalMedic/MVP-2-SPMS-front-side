cb468fafdedf2962470f25296c11b5bd177cd445
Мария Плетнева <=>
2024-03-05 15:57:07 +0500

    added cookie setter for testing purposes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,16 @@
+import Button from 'react-bootstrap/Button';
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
+
 const Home = () => {
-    return <h1>Home</h1>;
-  };
-  
-  export default Home;
+  const setCookies = () => {
+    cookies.set('professor_id', '1', { path: '/' });
+  }
+  return (<>
+    <h1>Home</h1>
+    <Button onClick={setCookies}>Set cookies</Button>
+  </>);
+};
+
+export default Home;
