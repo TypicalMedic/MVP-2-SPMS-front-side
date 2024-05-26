69c3c1e740e02529d0a28611a1b3a134af96d6cd
Мария Плетнева <=>
2024-05-26 18:30:53 +0500

    fixed incorrect map

--- src/pages/shared/status/StatusSelect.js
+++ src/pages/shared/status/StatusSelect.js
@@ -4,13 +4,16 @@ function StatusSelect(props) {
     return <>
         <Badge pill bg="info" className='mx-2 style-bg'>
             <select className={props.selectClass ? props.selectClass : 'style-select-in-badge'}>
-                {props.items ? props.items.map((item) => {
-                    item.name === props.status?
-                    <option className='style-option-in-badge' selected value={item.value}>{item.name}</option>:
-                    <option className='style-option-in-badge' value={item.value}>{item.name}</option>
-                }) : <>
-                    <option className='style-option-in-badge' >none</option>
-                </>}
+                {props.items ?
+                    props.items.map((item) => <>
+                        {
+                            item.name === props.status ?
+                                <option className='style-option-in-badge' selected value={item.value}>{item.name}</option> :
+                                <option className='style-option-in-badge' value={item.value}>{item.name}</option>
+                        }
+                    </>) : <>
+                        <option className='style-option-in-badge' >none</option>
+                    </>}
             </select>
         </Badge>
     </>
